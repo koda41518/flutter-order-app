06471bfe9d15f79bb6ce14f1df037e3e49f0c98d
koda41518 <155271337+koda41518@users.noreply.github.com>
2025-07-08 11:13:51 +0100

    feat: structure initiale avec écrans et widgets

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,27 +1,31 @@
 import 'package:flutter/material.dart';
+import 'sign_in_screen.dart';
 
 class SplashScreen extends StatelessWidget {
   const SplashScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    Future.delayed(const Duration(seconds: 2), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (_) => const SignInScreen()),
+      );
+    });
+
     return Scaffold(
-      backgroundColor: const Color(0xFFE3001B), // rouge comme sur la maquette
+      backgroundColor: const Color(0xFFFF002B),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Image.asset(
-              'assets/images/logo.png', // assure-toi que le nom correspond
-              width: 120,
-              height: 120,
-            ),
-            const SizedBox(height: 20),
+            Image.asset('assets/images/logo.png', width: 120),
+            const SizedBox(height: 16),
             const Text(
               'FoodMarket',
               style: TextStyle(
+                fontSize: 24,
                 color: Colors.white,
-                fontSize: 30,
                 fontWeight: FontWeight.bold,
               ),
             ),
