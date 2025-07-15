91831d083b2b040e6f946add3e9a5b0e20f6c49f
koda41518 <155271337+koda41518@users.noreply.github.com>
2025-07-15 15:09:06 +0100

    modification pour l apie

---
+++ lib/core/services/api_service.dart
@@ -0,0 +1,28 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+class ApiService {
+  static const String baseUrl = 'http://10.0.2.2:3000'; // Android Emulator
+
+  static Future<List<dynamic>> getRestaurants() async {
+    final response = await http.get(Uri.parse('$baseUrl/restaurants'));
+
+    if (response.statusCode == 200) {
+      return json.decode(response.body);
+    } else {
+      throw Exception('Failed to load restaurants');
+    }
+  }
+
+  static Future<void> postOrder(Map<String, dynamic> orderData) async {
+    final response = await http.post(
+      Uri.parse('$baseUrl/orders'),
+      headers: {'Content-Type': 'application/json'},
+      body: jsonEncode(orderData),
+    );
+
+    if (response.statusCode != 201) {
+      throw Exception('Failed to send order');
+    }
+  }
+}
--- lib/restaurant_data.dart
+++ lib/restaurant_data.dart
@@ -1,10 +1,21 @@
 import 'dart:convert';
 import 'package:flutter/services.dart';
 import 'core/models/restaut.dart';
+import 'core/services/api_service.dart';
 
-Future<List<Restaut>> loadRestaurantsFromJson() async {
-  final String jsonString = await rootBundle.loadString('assets/data/restaurants.json');
-  final List<dynamic> jsonData = json.decode(jsonString);
 
-  return jsonData.map((data) => Restaut.fromJson(data)).toList();
+
+Future<List<Restaut>> loadRestaurants() async {
+  try {
+    // Essayer l’API
+    final data = await ApiService.getRestaurants();
+    return data.map((json) => Restaut.fromJson(json)).toList();
+  } catch (e) {
+    print('⚠️ Erreur API, fallback sur JSON local : $e');
+
+    // Si erreur → lecture du fichier local
+    final String jsonString = await rootBundle.loadString('assets/data/restaurants.json');
+    final List<dynamic> jsonData = json.decode(jsonString);
+    return jsonData.map((data) => Restaut.fromJson(data)).toList();
+  }
 }
